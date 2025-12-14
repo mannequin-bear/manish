27a1a765a5d3fca50a8a16beb6307b4da20a420f
mannequin-bear <hirmanish04@gmail.com>
2025-12-14 12:59:15 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,7 +23,7 @@ export function Navbar() {
       <div className="max-w-7xl mx-auto px-4 md:px-8">
         <div className="flex items-center justify-between h-16">
           <a href="#" className="text-xl font-bold gradient-text">
-            Portfolio
+            My Space
           </a>
 
           {/* Desktop Navigation */}
